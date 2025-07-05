27590e12d5c6b9e6b3a75852529fcf38c26b090e
IANTSOGod <iantsochristianrazafindrazaka0gmail.com>
2025-07-05 10:49:44 +0300

    intergrationg better-auth session management

--- src/components/custom/Header.tsx
+++ src/components/custom/Header.tsx
@@ -1,21 +1,29 @@
 "use client";
 import Link from "next/link";
 import { Button } from "../ui/button";
-import { PlaneLanding } from "lucide-react";
+import { LogOut, PlaneLanding, User } from "lucide-react";
 import LoginComponent from "./LoginComponent";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
+import { authClient } from "@/lib/auth-client";
+import { Label } from "../ui/label";
 
 export default function Header() {
+  const session = authClient.useSession();
+  const unauthentifiedroute = [
+    "/register",
+    "/reset-password",
+    "/request-reset",
+  ];
   const [ishow, setisshow] = useState<Boolean>(true);
   const pathname = usePathname();
 
+  const logout = async () => {
+    await authClient.signOut();
+  };
+
   useEffect(() => {
-    if (
-      pathname === "/register" ||
-      pathname === "/reset-password" ||
-      pathname === "/request-reset"
-    ) {
+    if (pathname in unauthentifiedroute) {
       setisshow(false);
     } else {
       setisshow(true);
@@ -37,14 +45,29 @@ export default function Header() {
         <Link href={"/"}>Adventures</Link>
         <Link href={"/"}>Contact</Link>
       </div>
-      <div className="mt-[-5px] ml-auto md:flex hidden">
-        <LoginComponent></LoginComponent>
-        <Link href={"/register"}>
-          <Button className="w-[150px] bg-red-400 text-white hover:bg-red-700">
-            Register
-          </Button>
-        </Link>
-      </div>
+      {session?.data?.user ? (
+        <div className="ml-auto flex text-red-400  gap-x-2 ">
+          <User className="rounded-3xl bg-gray-100 w-[30px] h-[30px] p-1 border-1 border-red-400" />
+          <Label className="-mt-5 md:text-md font-bold md:flex hidden">
+            {session.data.user.name}
+          </Label>
+          <LogOut
+            className="!text-black cursor-pointer"
+            onClick={() => {
+              logout();
+            }}
+          ></LogOut>
+        </div>
+      ) : (
+        <div className="mt-[-5px] ml-auto md:flex hidden">
+          <LoginComponent></LoginComponent>
+          <Link href={"/register"}>
+            <Button className="w-[150px] bg-red-400 text-white hover:bg-red-700">
+              Register
+            </Button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
